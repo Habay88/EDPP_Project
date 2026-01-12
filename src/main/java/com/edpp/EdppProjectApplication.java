dc328db072208fa474fbd6c3ca16856c376b2025
Banjoko A.L <56545809+Habay88@users.noreply.github.com>
2026-01-12 17:19:19 +0100

    EDPP WALLET  PROJECT

---
+++ src/main/java/com/edpp/EdppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.edpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edpp/EdppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
